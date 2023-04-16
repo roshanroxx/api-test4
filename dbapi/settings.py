0dab8b81e263ba9ebbd34790884c8ce02a34f40e
roshanroxx <82316084+roshanroxx@users.noreply.github.com>
2023-04-16 18:52:20 +0530

    Update settings.py

--- dbapi/settings.py
+++ dbapi/settings.py
@@ -6,7 +6,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# DATABASE_URL = "postgresql://postgres:lzL3MdpsHhMLM8ykzQEz@containers-us-west-190.railway.app:5860/railway"
+DATABASE_URL = "postgresql://postgres:CKwOpcahwBymgArxgoct@containers-us-west-198.railway.app:7170/railway"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -68,15 +68,15 @@ WSGI_APPLICATION = 'dbapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
-#     'default': dj_database_url.config(default=DATABASE_URL,conn_max_age=1800),
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+DATABASES = {
+    'default': dj_database_url.config(default=DATABASE_URL,conn_max_age=1800),
+}
 
 
 # Password validation
